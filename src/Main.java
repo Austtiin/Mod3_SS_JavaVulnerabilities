60ab81b036625e3a5046f4d37f4ae71858925a04
Austtiin <austinstephens103@gmail.com>
2024-08-21 16:53:35 -0500

    Setup

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//main.java
+//main class to run the program
+
+
+//Austin Stephens
+//Ramsussen University
+//Professor Zayas
+//CEN4071C
+//08/21/2024
+
+//Week 03 - Assignment
+//Read through and follow instructions for each.
+
+//We will run the main class here however, seperate each problem into each class for readability.
+//Adding helper classes to do the work and then calling them in the main class.
+//Helper classes where neccecary.
+
+public class Main {
+
+    }
